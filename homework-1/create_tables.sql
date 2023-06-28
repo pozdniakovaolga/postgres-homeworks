e2b7fa564ec9b314da1ad63ea9437b13841a0fa0
pozdniakova olga <caramelca07@mail.ru>
2023-06-28 10:19:26 +0800

    Updated file

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,7 +1,6 @@
--- SQL-команды для создания таблиц
 CREATE TABLE employees
 (
-	employee_id int PRIMARY KEY,
+	employee_id serial PRIMARY KEY,
 	first_name varchar(20) NOT NULL,
 	last_name varchar(20) NOT NULL,
 	title varchar(50) NOT NULL,
@@ -18,7 +17,7 @@ CREATE TABLE customers
 
 CREATE TABLE orders
 (
-	order_id int PRIMARY KEY,
+	order_id serial PRIMARY KEY,
 	customer_id varchar(10) REFERENCES customers(customer_id),
 	employee_id int REFERENCES employees(employee_id),
 	order_date date NOT NULL,
